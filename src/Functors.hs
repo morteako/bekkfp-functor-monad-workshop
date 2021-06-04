542023cf0263148b0937413cd099e2bb9a4e8524
Morten Kolstad <morteako@student.matnat.uio.no>
2021-06-04 12:35:51 +0200

    .

--- src/Functors.hs
+++ src/Functors.hs
@@ -1,7 +1,18 @@
+{-# LANGUAGE KindSignatures #-}
+
 module Functors where
+
+import Prelude hiding (fmap, map)
 
 -- | Compute Fibonacci numbers
 -- >>> fib 5
 -- 30
 fib :: Int -> Int
 fib = id
+
+class MyFunctor (f :: * -> *) where
+  map :: (a -> b) -> f a -> f b
+
+instance MyFunctor Maybe where
+  map f Nothing = Nothing
+  map f (Just x) = Just (f x)
