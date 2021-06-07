559f88a1dacb7d0b034a93fbf77a074abfc93aaf
Morten Kolstad <morteako@student.matnat.uio.no>
2021-06-07 20:28:07 +0200

    functor-oppgaver basic done ish

--- src/Functors.hs
+++ src/Functors.hs
@@ -1,24 +1,55 @@
+{-# LANGUAGE InstanceSigs #-}
 {-# LANGUAGE KindSignatures #-}
 
 module Functors where
 
 import Prelude hiding (fmap, map)
 
--- | Compute Fibonacci numbers
--- >>> fib 5
--- 30
-fib :: Int -> Int
-fib = id
+class MyFunctor (f :: * -> *) where
+  fmap :: (a -> b) -> f a -> f b
 
--- class MyFunctor (f :: * -> *) where
---   fmap :: (a -> b) -> f a -> f b
+-- | OPPGAVE : implementer MyFunctor Maybe
+-- >>> fmap (+1) (Just 1)
+-- Just 2
+instance MyFunctor Maybe where
+  -- fmap :: Fyll inn og fjern kommentar
+  fmap = error "TODO"
 
--- class MyFunctor2 f where
---   fmap2 :: (a -> b) -> f a -> f b
+data OneOrTwo a = One a | Two a a
+  deriving (Show)
 
--- instance MyFunctor Maybe where
---   fmap f Nothing = Nothing
---   fmap f (Just x) = Just (f x)
+-- | OPPGAVE : implementer MyFunctor Maybe
+-- >>> fmap (+1) (Two 1 2)
+-- Two 2 3
+-- >>> fmap id (One 1)
+-- One 1
+data RemoteData e a
+  = NotAsked
+  | Loading
+  | Failure e
+  | Success a
+  deriving (Show)
 
--- instance MyFunctor (Either a) where
---   fmap = undefined
+-- | OPPGAVE : implementer MyFunctor for RemoteData
+-- >>> fmap reverse (Success "ABC")
+-- Success "CBA"
+-- >>> fmap reverse NotAsked
+-- NotAsked
+dataStuff :: [RemoteData String Int]
+dataStuff =
+  [ NotAsked,
+    Loading,
+    Failure "Gikk galt",
+    Success 0
+  ]
+
+dataStuffIsEven :: [RemoteData String Bool]
+dataStuffIsEven = error "TODO"
+
+-- | OPPGAVE : bruk det du har lært om functors til å gjør om dataStuff til det som står i testen.
+-- >>> dataStuffIsEven
+-- [ NotAsked,
+--     Loading,
+--     Failure "Gikk galt",
+--     Success True
+--   ]
