412f19304276e40b891a3b5c9f173b719d51c1b5
Morten Kolstad <morteako@student.matnat.uio.no>
2021-06-11 09:52:06 +0200

    start

--- src/Intro.hs
+++ src/Intro.hs
@@ -1,5 +1,18 @@
 module Intro where
 
+
+-- | Compute Fibonacci numbers
+--
+-- Examples:
+--
+-- >>> fib 10
+-- 55
+--
+-- >>> fib 5
+-- 5
+fib :: Int -> Int
+fib = error "todo fib"
+
 data RemoteData e a
   = NotAsked
   | Loading
